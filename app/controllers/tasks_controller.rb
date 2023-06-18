b68bcc85208d6cd5aaf7295984f7505737ee92e5
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-06-18 17:10:15 +0300

    Create tasks_controller.rb

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :set_project
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = @project.tasks
+  end
+
+  def show
+  end
+
+  def new
+    @task = @project.tasks.new
+  end
+
+  def create
+    @task = @project.tasks.new(task_params)
+
+    if @task.save
+      redirect_to project_tasks_path(@project), notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to project_tasks_path(@project), notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to project_tasks_path(@project), notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
